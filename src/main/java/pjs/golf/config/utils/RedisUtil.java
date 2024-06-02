8a5d4ed8f1ba63e30e371dec50dab9b71f40142c
bavuchoko <bavuchoko@naver.com>
2024-06-02 20:40:59 +0900

    redis가 원하는 동작을 안하는거 같아서 로그 출력

--- src/main/java/pjs/golf/config/utils/RedisUtil.java
+++ src/main/java/pjs/golf/config/utils/RedisUtil.java
@@ -1,6 +1,8 @@
 package pjs.golf.config.utils;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -11,16 +13,20 @@ import java.time.Duration;
 @Component
 public class RedisUtil {
 
+    private final Logger log = LoggerFactory.getLogger(RedisUtil.class);
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     public String getData(String key){
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        log.info("get key ={ }" , key);
         return valueOperations.get(key);
     }
 
     public void setData(String key, String value){
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        log.info("key ={ }" , key);
+        log.info("value ={ }" , value);
         valueOperations.set(key,value);
     }
 
@@ -31,6 +37,7 @@ public class RedisUtil {
     }
 
     public void deleteData(String key){
+        log.info("delete key ={ }" , key);
         stringRedisTemplate.delete(key);
     }
 
