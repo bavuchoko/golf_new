ea33449cd94d07cbd08da6f4ac1bb378e21a54a0
pjs <bavuchoko@naver.com>
2024-07-21 14:56:50 +0900

    objectmapper 필요없어서 제거

--- src/main/java/pjs/golf/config/AppConfig.java
+++ src/main/java/pjs/golf/config/AppConfig.java
@@ -1,8 +1,5 @@
 package pjs.golf.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -13,13 +10,5 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return objectMapper;
     }
 }
